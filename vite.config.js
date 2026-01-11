ebcb7e3bb8821d0b5802b040ed55d6b6ab4dfa04
ksy <tndud4615@gmail.com>
2026-01-11 15:09:41 +0900

    docs file

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/clone-airbnb/', // 본인의 레포지토리 이름
+  build: {
+    outDir: 'docs' // 빌드 결과물을 dist가 아닌 docs 폴더에 생성.
+  },
   plugins: [
     vue(),
     vueDevTools(),
