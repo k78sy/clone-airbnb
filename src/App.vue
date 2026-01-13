7288e47817a4ff04d97a502bac3d563de9bdfabf
ksy <tndud4615@gmail.com>
2026-01-13 12:49:24 +0900

    데이터 통신

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,43 @@
 //import { RouterLink, RouterView } from 'vue-router'
 import HeaderTop from '@/components/HeaderTop.vue'
 import FooterBottom from '@/components/FooterBottom.vue'
-import List from '@/components/List.vue'
+import ListSlider from '@/components/ListSlider.vue'
+
+import { onMounted, reactive } from 'vue' //{ 상수,변수,함수가 넘어온다 }
+import axios from 'axios' //객체 주소값 리턴
+
+const state = reactive({
+  sections: [],
+})
+
+onMounted(async () => {
+  axios.get('airbnb.json').then((res) => {
+    state.sections = res.data.sections
+    console.log(state.sections)
+  })
+})
 </script>
 
 <template>
   <header-top></header-top>
-  <list></list>
-  <list></list>
-  <list></list>
+
+  <main>
+    <section v-for="section in state.sections" :key="section.sectionId">
+      <div class="slider-top">
+        <a href="">{{ section.sectionData.title }}</a>
+        <div class="slider-button">
+          <button class="arrow-btn btn-arctive">
+            <font-awesome-icon :icon="['fas', 'angle-left']" />
+          </button>
+          <button class="arrow-btn"><font-awesome-icon :icon="['fas', 'angle-right']" /></button>
+        </div>
+      </div>
+      <list-slider :foo="section.sectionData"></list-slider>
+    </section>
+  </main>
+
+
+
   <footer-bottom></footer-bottom>
 </template>
 
---
+++ src/components/ListSlider.vue
@@ -0,0 +1,126 @@
+<script setup>
+const props = defineProps(['foo'])
+console.log(props.foo)
+// foo = section.sectionData
+</script>
+<template>
+  <div class="slider-list">
+    <div v-for="item in foo.items" class="slide-item" :key="item.contextualPictures[0].id">
+      <div class="item-img">
+        <img :src="item.contextualPictures[0].picture" />
+      </div>
+      <ul class="item-info">
+        <li class="item-location">{{ item.title }}</li>
+        <li class="item-date">{{ item.structuredContent.primaryLine[0].body }}</li>
+        <li class="">
+          <span class="item-period">
+            {{item.structuredDisplayPrice.primaryLine.accessibilityLabel}}
+          </span>
+          <span class="item-rating">{{ item.avgRatingLocalized }}</span>
+        </li>
+      </ul>
+      <div class="item-icon">
+        <span v-if="item.badges.length" class="icon-cate">{{ item.badges[0].text }}</span>
+        <span class="icon-heart"><font-awesome-icon :icon="['far', 'heart']" /></span>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+section {
+  max-width: $width-pc;
+  padding: $pd;
+  .slider-top {
+    display: flex;
+    justify-content: space-between;
+    padding-bottom: 20px;
+    a {
+      text-decoration: none;
+      color: #000;
+      font-size: 1.2em;
+      font-weight: 500;
+    }
+    .slider-button {
+    }
+  }
+}
+.slider-list {
+  display: grid;
+  gap: 15px;
+  grid-template-columns: repeat(7, 1fr);
+  .slide-item {
+    position: relative;
+    .item-img {
+      border-radius: 20px;
+      overflow: hidden;
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+    .item-info {
+      padding: 7px 5px;
+      display: flex;
+      flex-direction: column;
+      gap: 5px;
+      li {
+        color: #999;
+        font-size: 0.9em;
+        &.item-location {
+          font-size: 1em;
+          font-weight: 500;
+          color: #000;
+        }
+        &.item-date {
+        }
+        span {
+          &:not(:first-child) {
+            padding-left: 10px;
+            position: relative;
+            &:before {
+              position: absolute;
+              content: '';
+              background: #999;
+              width: 2px;
+              height: 2px;
+              left: 5px;
+              top: 50%;
+              transform: translateY(-50%);
+            }
+          }
+        }
+        .item-price {
+        }
+        .item-period {
+        }
+        .item-rating {
+        }
+      }
+    }
+    .item-icon {
+      position: absolute;
+      top: 0;
+      display: flex;
+      width: 100%;
+      justify-content: space-between;
+      padding: 15px;
+      box-sizing: border-box;
+      .icon-cate {
+        font-size: 0.7em;
+        font-weight: 600;
+        background: rgba(255, 255, 255, 0.7);
+        border: 1px solid #fff;
+        border-radius: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 5px 8px;
+      }
+      .icon-heart {
+        color: #fff;
+      }
+    }
+  }
+}
+</style>
